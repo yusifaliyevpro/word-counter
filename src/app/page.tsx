d5b8da4fe72cb34201854165b8dc46ea7c5ec2a1
Yusif Aliyev <114253515+YusifAliyevPro@users.noreply.github.com>
2025-01-08 22:00:35 +0400

    neeeew

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,17 @@ import { useState } from "react";
 
 export default function Home() {
   const [text, setText] = useState("");
+  if (text === "YusifAliyevPro")
+    return (
+      <main className="flex min-h-screen w-full flex-col items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500 px-56 transition-all">
+        <embed
+          src="/1.pdf"
+          width="700"
+          height="505"
+          type="application/pdf"
+        ></embed>
+      </main>
+    );
   return (
     <main className="flex min-h-screen w-full flex-col items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500 px-56 transition-all">
       <div className="flex min-h-96 w-full flex-col rounded-xl bg-white bg-opacity-80 shadow-medium backdrop-blur-sm">
